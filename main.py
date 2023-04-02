50d30dbb07a2f4886c084593179e0686b8b6ae57
Karol Kruszyński <karol.kruszynski0@gmail.com>
2023-04-02 18:20:04 +0200

    Create check_player_won() and while loop of the game

--- main.py
+++ main.py
@@ -5,16 +5,26 @@
 # todo: If player guess the number then win
 
 import random
-
+PLAYER_WON = False
 def randomizer():
-    number = random.randint(1,100)
+    # number = random.randint(1,100)
+    number = 10
     return number
 def checker(number,player_number):
-    if number == player_number:
-        print("yes!")
+    if player_number > number:
+        print("Too high!")
+    elif player_number < number:
+        print("Too low!")
+    elif player_number == number:
+        print(f"You won ! The hidden number is {number}")
+        return True
+def check_player_won(number,player_number):
+    global PLAYER_WON
+    PLAYER_WON = checker(number,player_number)
 def player_guess():
     player_number = int(input(" Make a guess: "))
     return player_number
-number = randomizer()
-player_number = player_guess()
-checker(number,player_number)
+while not PLAYER_WON:
+    number = randomizer()
+    player_number = player_guess()
+    check_player_won(number,player_number)
