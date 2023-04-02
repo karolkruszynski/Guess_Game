3b1194102d0fd2704ce8569d4cda3a143defd073
Karol Kruszyński <karol.kruszynski0@gmail.com>
2023-04-02 17:34:17 +0200

    Make TODO list

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-print("Initial Commit")
+# todo: Making random number 1 to 100 to guess
+# todo: Make difficulty level choose easy = 10 attempts and hard = 5 attempts
+# todo: Make funct that tell player that he/she bet to low or to high
+# todo: If player don't guess his number of attempts decrease, after last try player lose
+# todo: If player guess the number then win
