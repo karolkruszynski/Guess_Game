5a8115f1c6a637b746980a6967f2adf922740282
Karol Kruszyński <karol.kruszynski0@gmail.com>
2023-04-04 19:10:09 +0200

    Add: Difficulty level validation, if input is different than 'easy' or 'hard' program is shout down. Add: User info about attempts on each level. Add: User info about what was the number when user lose the game. Add: Comments to documentation in code. Bug fix: After every guess new number was generated. FIX

--- main.py
+++ main.py
@@ -5,14 +5,22 @@
 # todo: If player guess the number then win
 
 import random
+
+'''Welcome print's'''
+
 print("Welcome to the Number Guessing Game!")
 print("I'm thinking of a number between 1 and 100.")
+'''GLOBAL VARIABLES '''
 PLAYER_WON = False
-# PLAYER_LIFE = 0
+NUMBER_RAND = random.randint(1, 100)
+
+'''Store and return every iteration the same number which was choose ealier above.'''
 def randomizer():
-    number = random.randint(1,100)
-    # number = 10
+    global NUMBER_RAND
+    number = NUMBER_RAND
     return number
+
+'''Checking player number with choosed number and player life control with info about attempts left. Also return True to main game loop when player is ran out of lifes'''
 def checker(number,player_number):
     global PLAYER_LIFE
     if player_number > number:
@@ -20,7 +28,7 @@ def checker(number,player_number):
         print("Guess again.")
         PLAYER_LIFE -= 1
         if PLAYER_LIFE == 0:
-            print("You ran out of attempts! You LOST!")
+            print(f"You ran out of attempts! You LOSE! The number was {number}")
             return True
         print(f"You have {PLAYER_LIFE} attempts remaining to guess the number")
     elif player_number < number:
@@ -28,28 +36,36 @@ def checker(number,player_number):
         print("Guess again.")
         PLAYER_LIFE -= 1
         if PLAYER_LIFE == 0:
-            print("You ran out of attempts! You LOST!")
+            print(f"You ran out of attempts! You LOSE! The number was {number}")
             return True
         print(f"You have {PLAYER_LIFE} attempts remaining to guess the number")
     elif player_number == number:
         print(f"You won ! The hidden number was {number}")
         return True
+
+'''Global var PLAYER_WON store the boolean value for main game loop'''
 def check_player_won(number,player_number):
     global PLAYER_WON
     PLAYER_WON = checker(number,player_number)
+
+'''player_number store and catch every player guess and return to the main loop.'''
 def player_guess():
     player_number = int(input(" Make a guess: "))
     return player_number
 
+'''Catching and storing choosen diff level and decide which value will be in GLOBAL PLAYER_LIFE depends of level'''
 difficulty_level = input("Choose a difficulty level! Type 'easy' or 'hard': ").lower()
 if difficulty_level == "easy":
     PLAYER_LIFE = 10
+    print(f"You have {PLAYER_LIFE} attempts at level EASY")
 elif difficulty_level == "hard":
     PLAYER_LIFE = 5
+    print(f"You have {PLAYER_LIFE} attempts at level HARD")
 else:
-    print("You write invalid syntax!")
+    print("You write invalid syntax! Please restart game and type 'easy' or 'hard' ")
 
-while not PLAYER_WON:
+'''Main Game Loop with validation if PLAYER_WON is diff than False, and call the rest of funct'''
+while not PLAYER_WON and difficulty_level == "easy" or difficulty_level == "hard":
     number = randomizer()
     player_number = player_guess()
     check_player_won(number,player_number)
